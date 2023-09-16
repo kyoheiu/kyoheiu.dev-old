14d51b5148e16e6effe3ec7adc36b19b51b1f320
Kyohei Uto <im@kyoheiu.dev>
2023-09-17 05:01:39 +0900

    rewrite in SvelteKit

---
+++ src/lib/Link.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+    import {GithubSolid, CodeBranchOutline, PenNibOutline, PenOutline, CaptioningOutline, DiscordSolid, MailBoxOutline} from 'flowbite-svelte-icons';
+
+	type Link = {
+		name: string;
+		link: string;
+	};
+
+	const links: Link[] = [
+		{
+			name: 'GitHub',
+			link: 'https://github.com/kyoheiu'
+		},
+		{
+			name: 'sourcehut',
+			link: 'https://git.sr.ht/~kyoheiu/'
+		},
+		{
+			name: 'blog (ja)',
+			link: 'https://blog.kyoheiu.dev'
+		},
+		{
+			name: 'zenn (ja)',
+			link: 'https://zenn.dev/kyoheiu'
+		},
+		{
+			name: 'TED Translation',
+			link: 'https://www.ted.com/profiles/14244197/translator'
+		},
+		{ name: 'Discord@kyoheiu', link: '' },
+		{ name: 'im@kyoheiu.dev', link: '' }
+	];
+</script>
+
+{#each links as link}
+	<a
+		href={link.link}
+		class="flex flex-row items-center justify-center text-slate-800 bg-slate-200 rounded-md w-48 h-10 m-1 p-2"
+	>
+		{#if link.name === 'GitHub'}
+        <GithubSolid />
+		{:else if link.name === 'sourcehut'}
+        <CodeBranchOutline />
+		{:else if link.name === 'blog (ja)'}
+        <PenNibOutline />
+		{:else if link.name === 'zenn (ja)'}
+        <PenOutline />
+		{:else if link.name === 'TED Translation'}
+        <CaptioningOutline />
+		{:else if link.name === 'Discord@kyoheiu'}
+        <DiscordSolid />
+		{:else if link.name === 'im@kyoheiu.dev'}
+        <MailBoxOutline />
+		{/if}
+		&nbsp;
+		<span class="text-slate-800 break-all">
+			{link.name}
+		</span>
+	</a>
+{/each}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import "../app.css";
+  import Links from '$lib/Link.svelte';
+</script>
+
+<img src="images/avatar.png" class="rounded-full" alt="my avatar" width="150" height="150" />
+
+<!-- Title -->
+<h1 class="text-2xl m-2">Kyohei Uto</h1>
+
+<!-- Short Bio -->
+<div class="text-center m-2 mb-6 w-48">
+	Husband, father, editor, working student@UoL, and developer in the wild.
+</div>
+
+<Links />
+
+<div class="text-center mt-6 mb-2 w-48">
+	Powered by <a class="underline" href="https://astro.build/" rel="noopener">Astro</a>, illustration
+	by my wife.
+</div>
